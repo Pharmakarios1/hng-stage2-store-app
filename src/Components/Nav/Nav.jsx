39748247d730edd3d6b92f6b84ead2bc0c802665
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-07-09 16:39:22 +0100

    app store

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,13 +2,16 @@ import "./Nav.css";
 import { alpha_logo2, cart, heart, menu, help } from "../../assets";
 import { CiSearch } from "react-icons/ci";
 import { FaAngleDown } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div className="header">
       <div className="header-wrapper">
         <div className="logo">
-          <img src={alpha_logo2} alt="alpha-logo" />
+          <Link to="/">
+            <img src={alpha_logo2} alt="alpha-logo" />
+          </Link>
         </div>
         <div className="center">
           <div className="search">
@@ -28,7 +31,12 @@ const Nav = () => {
         </div>
         <div className="menu">
           <img src={heart} alt="likee" />
-          <img src={cart} alt="cart" />
+          <div className="cart">
+            <Link to="/cart">
+              <img src={cart} alt="cart" />
+              <span className="cart-counter">4</span>
+            </Link>
+          </div>
           <img src={menu} alt="menu" />
         </div>
       </div>
