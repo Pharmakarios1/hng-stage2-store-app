7adff7e7e884a3bafa27fe0a8769a1f9e64b5fcb
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-07-10 14:07:13 +0100

    app store

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -1,7 +1,15 @@
-import Nav from "../Components/Nav/Nav";
+import { Link } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
 import "../Components/ProductHome/Checkout.css";
-import { alpha_logo2 } from "../assets";
+import {
+  alpha_logo2,
+  visaLogo,
+  PayPal,
+  verve,
+  master,
+  chair1,
+} from "../assets";
+import { FaAngleRight } from "react-icons/fa";
 
 const CheckOut = () => {
   return (
@@ -9,6 +17,52 @@ const CheckOut = () => {
       <div className="checkout-header">
         <img src={alpha_logo2} alt="" />
         <h4>CheckOut</h4>
+      </div>
+      <div className="bread-crumb-cart">
+        home <FaAngleRight /> Furniture <FaAngleRight /> sofa <FaAngleRight />
+        LuxeComfort Sectional Sofa <FaAngleRight /> Cart
+      </div>
+      <div className="product-detail-wrapper">
+        <div className="product-top">
+          <div className="product-img">Lets take your Order</div>
+          <div className="cart-desc-checkout">
+            <div className="cart-desc-flex">
+              <h4>ORDER DETAILS</h4>
+              <h4>Update Cart</h4>
+            </div>
+            <div className="cart-desc-flex-item">
+              <img src={chair1} alt="" />
+              <div>
+                <h4>LuxeComfort Sectional Sofa</h4>
+                <p>1 Seater Sofa (Sand Beige)</p>
+                <h4>$3500</h4>
+                <p>Quantity: 1</p>
+                <span>+Delivery fee $20</span>
+              </div>
+            </div>
+            <div className="cart-desc-item-total">
+              <div className="delivery-fee">
+                <p>Delivery fee</p>
+                <p>$3500</p>
+              </div>
+              <div className="delivery-fee">
+                <p>Subtotal</p>
+                <p>$20</p>
+              </div>
+              <div className="delivery-fee">
+                <p>Total</p>
+                <p>$3520</p>
+              </div>
+            </div>
+            <div className="cart-desc-total">
+              <h4>Total</h4>
+              <p>$3,900</p>
+            </div>
+            <button className="btn-checkout">
+              <Link to="/checkout">Continue to CheckOut</Link>
+            </button>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
